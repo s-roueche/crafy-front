6fc3d250c7297950fd2c1ef419030ee19be06fe5
mchiarello <m.chiarello1854@gmail.com>
2025-05-27 22:59:51 +0200

    used tanstackQuery and axios for query

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import {useEffect, useState} from 'react'
+import {Button} from "@heroui/react";
+import {Gi3DMeeple } from 'icons-react/gi';
 import './App.css'
+import {useQuery} from "@tanstack/react-query";
+import axios from 'axios';
 
 function App() {
-    const [data, setData] = useState('')
+    async function getHello() {
+        const response = await axios.get('http://localhost:3000');
+        return response.data;
+    }
+    
+    const { isPending, isError, data, error } = useQuery({
+        queryKey: ['helloWorld'],
+        queryFn: getHello
+    });
 
-    useEffect(() => {
-        fetch('http://localhost:3000')
-            .then(res => res.text())
-            .then(setData)
-            .catch(console.error);
-    }, [])
+    if (isPending) {
+        return <span>Loading...</span>;
+    }
 
-  return (
-    <div>
-        {data}
-    </div>
-  )
+    if (isError && error instanceof Error) {
+        return <span>Error: {error.message}</span>;
+    }
+    
+    return (
+        <>
+            <h1 className="text-3xl font-bold">
+                {data}
+            </h1>
+            <Gi3DMeeple />
+            <Button color={"primary"}>Button</Button>
+        </>
+    );
 }
 
 export default App
