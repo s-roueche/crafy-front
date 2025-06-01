d8922799edfd22f588afe3524d0a1e5d6f10f1fe
mchiarello <m.chiarello1854@gmail.com>
2025-06-01 20:04:19 +0200

    new queries

--- src/routes/feed.tsx
+++ src/routes/feed.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import {useQuery} from "@tanstack/react-query";
 import {getPublishedPosts} from "../queries/getQueries.tsx";
-import type { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
+import type { Key, ReactElement, ReactNode, ReactPortal } from 'react';
 
 export const Route = createFileRoute('/feed')({
     component: RouteComponent,
@@ -25,7 +25,7 @@ function RouteComponent() {
     
     return (
         <>
-            {data.map((post: { id: Key | null | undefined; title: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; content: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) => (
+            {data.map((post: { id: Key | null | undefined; title: string | number | bigint | boolean | ReactElement | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement | Iterable<ReactNode> | null | undefined> | null | undefined; content: string | number | bigint | boolean | ReactElement | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement | Iterable<ReactNode> | null | undefined> | null | undefined; }) => (
                 <div key={post.id}>
                     <h2>{post.title}</h2>
                     <p>{post.content}</p>
